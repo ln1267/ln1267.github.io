27528f4f644a6a196675a62c8232795bf78b0864
Ning <ln1267@gmail.com>
2021-12-07 15:38:38 -0500

    add get season function

--- _includes/Functions_NL.R
+++ _includes/Functions_NL.R
@@ -112,6 +112,22 @@ f_Pbias=function(obs,sim){
 },
 
 
+## Function for get season
+f_Season = function(DATES) {
+    WS <- as.Date("2012-12-15", format = "%Y-%m-%d") # Winter Solstice
+    SE <- as.Date("2012-3-15",  format = "%Y-%m-%d") # Spring Equinox
+    SS <- as.Date("2012-6-15",  format = "%Y-%m-%d") # Summer Solstice
+    FE <- as.Date("2012-9-15",  format = "%Y-%m-%d") # Fall Equinox
+
+    # Convert dates from any year to 2012 dates
+    d <- as.Date(strftime(DATES, format="2012-%m-%d"))
+
+    ifelse (d >= WS | d < SE, "Winter",
+            ifelse (d >= SE & d < SS, "Spring",
+                    ifelse (d >= SS & d < FE, "Summer", "Fall")))
+  },
+
+
 #
 ## Function for calculating Hamon PET----
 #' @param tavg Mean temperature.
