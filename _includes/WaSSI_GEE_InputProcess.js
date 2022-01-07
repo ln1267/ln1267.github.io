ddaeb75cb1617d3bf0c8f5b10e30046f33a7547c
Ning <ln1267@gmail.com>
2022-01-07 13:28:17 -0500

    add zonal by group for HUC and LC zonal

--- _includes/WaSSI_GEE_InputProcess.js
+++ _includes/WaSSI_GEE_InputProcess.js
@@ -43,9 +43,7 @@ OUTPUTS:
           - 'LST': land surface temperature
 */
 
-
-// ---------------------Functions ------------------------------------
-
+// Process original data to the target---------------------------------
 function namerange(yr){
   
   var days=['001','009','017','025','033','041','049','057','065','073','081','089','097','105','113','121','129','137','145','153','161','169','177','185','193','201','209','217','225','233','241','249','257','265','273','281','289','297','305','313','321','329','337','345','353','361']
@@ -55,54 +53,8 @@ function namerange(yr){
   return ans
 }  
 
-// Make an image from shp id.
-exports.RasterizeWS=function(shp,field){
-var StateImg = shp
-  .reduceToImage({
-    properties: [field],
-    reducer: ee.Reducer.first()
-});
-return StateImg;
-}
-
-// Zonal image by Bands
-exports.zonalImg=function(img,Watersheds,scale){
-  
-  var table=img.reduceRegions(Watersheds, ee.Reducer.mean(), scale, "EPSG:4326");
-
-  table = table
-        .map(function(feature) {
-           return ee.Feature(feature.select([".*"], null, false))
-        });
-		
-	return table;
-}	
-
-// Zonal image collections
-exports.zonalTS=function(imgcol,Watersheds,startdate,enddate){
-  
-  var table = imgcol
-      .filterDate(startdate,enddate)
-      .map(function(img) {
-        // Add the date as a number.
-        var dateStr = img.date();
-        var dateNum = ee.Number.parse(dateStr.format("YYYYMMdd"));
-        img = img.addBands(ee.Image(dateNum).rename('date'));
-        return img.reduceRegions(Watersheds, ee.Reducer.mean(), 100, "EPSG:4326")
-      }).flatten();
-
-  table = table
-        .map(function(feature) {
-  return ee.Feature(feature.select([".*"], null, false));
-});
-
-  return table
-}
-
 
 var  LAIbands=['b1','b2','b3','b4','b5','b6','b7','b8','b9','b10','b11','b12','b13','b14','b15','b16','b17','b18','b19','b20','b21','b22','b23','b24','b25','b26','b27','b28','b29','b30','b31','b32','b33','b34','b35','b36','b37','b38','b39','b40','b41','b42','b43','b44','b45','b46']
-
-// Process original data to the target---------------------------------
 
 // Merge 8days LAI all Time series 
 exports.LAI_8days_2000_2020=ee.Image('projects/ee-ln1267/assets/LAI_USA_8days_2000')
@@ -148,6 +100,7 @@ exports.LAI_8days_2000_2020=ee.Image('projects/ee-ln1267/assets/LAI_USA_8days_20
         .addBands(ee.Image('projects/ee-ln1267/assets/LAI_USA_8days_2020')
         .select(LAIbands,namerange(2020)))
 
+// ------------------dataset-------------------
 
 // Mosia impervious for USA and MX
 var Imp_USA = ee.Image('projects/ee-ln1267/assets/NLCD_2016_Impervious')
@@ -167,4 +120,103 @@ exports.SOIL_SAC=ee.Image("users/ln1267/USA_WaSSI/soil_WaSSI_stacks_USA")
 exports.LAI_avg=ee.Image("users/ln1267/USA_WaSSI/US_lai_monthly_avg_2000_2019")
         .select(['b1','b2','b3','b4','b5','b6','b7','b8','b9','b10','b11','b12'],
         ['01','02','03','04','05','06','07','08','09','10','11','12'])
+
+
+
+
+
+// ------------------Functions for Zonal Img-------------------
+
+// Make an image from shp id.
+exports.RasterizeWS=function(shp,field){
+var StateImg = shp
+  .reduceToImage({
+    properties: [field],
+    reducer: ee.Reducer.first()
+});
+return StateImg;
+}
+
+// Zonal image by Bands
+exports.zonalImg=function(img,Watersheds,scale){
+  
+  var table=img.reduceRegions(Watersheds, ee.Reducer.mean(), scale, "EPSG:4326");
+
+  table = table
+        .map(function(feature) {
+           return ee.Feature(feature.select([".*"], null, false))
+        });
+		
+	return table;
+}	
+
+// Zonal image collections
+exports.zonalTS=function(imgcol,Watersheds,startdate,enddate){
+  
+  var table = imgcol
+      .filterDate(startdate,enddate)
+      .map(function(img) {
+        // Add the date as a number.
+        var dateStr = img.date();
+        var dateNum = ee.Number.parse(dateStr.format("YYYYMMdd"));
+        img = img.addBands(ee.Image(dateNum).rename('date'));
+        return img.reduceRegions(Watersheds, ee.Reducer.mean(), 100, "EPSG:4326")
+      }).flatten();
+
+  table = table
+        .map(function(feature) {
+  return ee.Feature(feature.select([".*"], null, false));
+});
+
+  return table
+}
+
+// ---------------------------Functions for zonal by group---------------------------
+
+// Convert bands to img collection 
+exports.bandToCollection = function(collection){
+  // get band names
+  var bands =  collection.bandNames()
+  var dayCounter = ee.List.sequence(1, bands.size())
+
+  // build new collection with 1 image per band
+  var newCollection = dayCounter.map(function(b){
+  
+  var img = ee.Image(collection.select(ee.String(bands.get(ee.Number(b).subtract(1)))))
+  
+  return(img.set('system:index',bands.get(ee.Number(b).subtract(1))))
+
+  })
+
+  return newCollection
+}
+
+
+// Zonal data by HUC and LC
+exports.zonalGroupLAI=function(img){
+  
+  // huc_lc (image) and Region (feature) are required
+  
+  var nlDiff = img.addBands(huc_lc);
+
+  // Grouped a mean reducer: change of nightlights by land cover category.
+  var means = nlDiff.reduceRegion({
+    reducer: ee.Reducer.mean().group({
+      groupField: 1,
+      groupName: 'code',
+    }),
+    geometry: Region,
+    scale: 100,
+    maxPixels: 1e13
+  });
+  
+  return(ee.Feature(null,means));
+}
+
+/* example
+
+var lai_avg_zonal=ee.ImageCollection(bandToCollection(LAI_avg))
+            .map(zonalGroupLAI);
+
+*/
 
