f78c755245bac3a64240df22b22d575c90447438
Ning <ln1267@gmail.com>
2022-03-16 23:00:08 -0400

    add ggplot themes

--- _includes/Functions_NL.R
+++ _includes/Functions_NL.R
@@ -10,6 +10,67 @@
 #	f_plot<-function(data,info,annual=FALSE,monthly=FALSE) for monthly or annual grid data plot
 #	f_grid2basin(data,type="annual",fun="mean")	#Transfer grid frame data to basin data by fun="mean"
 
+# Theme for ggplot ----
+theme_ning<-function(size.axis=8,size.title=10,base_family="sans"){
+  theme_bw(base_family = base_family) %+replace%
+  theme(axis.title = element_text(face="bold", colour="black", size=size.title),
+        axis.text= element_text(angle=0, vjust=0.3, size=size.axis),
+        legend.title = element_text(colour="black", size=size.axis, face="bold"),
+        legend.text = element_text(colour="black", size = size.axis),
+        strip.text.x = element_text(size = size.axis,margin=margin(4, 2, 6, 2), face="bold"),
+        strip.text.y = element_text(size = size.axis,margin=margin(4, 2, 4, 6), face="bold",angle=-90),
+        legend.key.size=unit(1.2, "lines"),
+        legend.box.spacing=unit(1, "mm"),
+        strip.background = element_blank(),
+        plot.title = element_text(vjust = 2.5,hjust = 0.5,face="bold")
+  )
+}
+
+theme_Publication <- function(base_size=14, base_family="sans") {
+      library(grid)
+      library(ggthemes)
+      (theme_foundation(base_size=base_size, base_family=base_family)
+       + theme(plot.title = element_text(face = "bold",
+                                         size = rel(1.2), hjust = 0.5, margin = margin(0,0,20,0)),
+               text = element_text(),
+               panel.background = element_rect(colour = NA),
+               plot.background = element_rect(colour = NA),
+               panel.border = element_rect(colour = NA),
+               axis.title = element_text(face = "bold",size = rel(1)),
+               axis.title.y = element_text(angle=90,vjust =2),
+               axis.title.x = element_text(vjust = -0.2),
+               axis.text = element_text(), 
+               axis.line.x = element_line(colour="black"),
+               axis.line.y = element_line(colour="black"),
+               axis.ticks = element_line(),
+               panel.grid.major = element_line(colour="#f0f0f0"),
+               panel.grid.minor = element_blank(),
+               legend.key = element_rect(colour = NA),
+               legend.position = "bottom",
+               legend.direction = "horizontal",
+               legend.box = "vetical",
+               legend.key.size= unit(0.5, "cm"),
+               #legend.margin = unit(0, "cm"),
+               legend.title = element_text(face="italic"),
+               plot.margin=unit(c(10,5,5,5),"mm"),
+               strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
+               strip.text = element_text(face="bold")
+       ))
+      
+}
+
+scale_fill_Publication <- function(...){
+      library(scales)
+      discrete_scale("fill","Publication",manual_pal(values = c("#386cb0","#f87f01","#7fc97f","#ef3b2c","#feca01","#a6cee3","#fb9a99","#984ea3","#8C591D")), ...)
+      
+}
+
+scale_colour_Publication <- function(...){
+      library(scales)
+      discrete_scale("colour","Publication",manual_pal(values = c("#386cb0","#f87f01","#7fc97f","#ef3b2c","#feca01","#a6cee3","#fb9a99","#984ea3","#8C591D")), ...)
+      
+}
+
 
 funs_nl<-list(
 
@@ -33,22 +94,6 @@ f_lib_check=function(libs){
   a<-lapply(libs, require, character.only = TRUE)
 },
 
-# Theme for ggplot ----
-theme_ning=function(p1,size.axis=8,size.title=10,base_family="sans"){
-p1+
-  theme_bw(base_family = base_family) %+replace%
-  theme(axis.title = element_text(face="bold", colour="black", size=size.title),
-        axis.text= element_text(angle=0, vjust=0.3, size=size.axis),
-        legend.title = element_text(colour="black", size=size.axis, face="bold"),
-        legend.text = element_text(colour="black", size = size.axis),
-        strip.text.x = element_text(size = size.axis,margin=margin(4, 2, 6, 2), face="bold"),
-        strip.text.y = element_text(size = size.axis,margin=margin(4, 2, 4, 6), face="bold",angle=-90),
-        legend.key.size=unit(1.2, "lines"),
-        legend.box.spacing=unit(1, "mm"),
-        strip.background = element_blank(),
-        plot.title = element_text(vjust = 2.5,hjust = 0.5,face="bold")
-  )
-},
 
 f_digits=function(x,n=2,format=F) {
   if(format){
